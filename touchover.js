11c39af26dc868c227038f796c7f311a45ed27d3
Max <61327744+adammaks@users.noreply.github.com>
2022-08-21 18:11:57 +0300

    Add files via upload

---
+++ touchover.js
@@ -0,0 +1,9 @@
+function myFunction()
+{
+    document.getElementById('swap').setAttribute("class", "style1");
+}
+
+function myFunctions()
+{
+    document.getElementById('swap').setAttribute("class", "style2");
+}
